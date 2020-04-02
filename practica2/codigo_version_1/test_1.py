e11a4083205c663c159ce198f7a082bfa43cc398
Angel Murcia Diaz <angel.ns.333@gmail.com>
2020-04-02 21:58:43 +0200

    Modificada ruta de modelos

--- practica2/codigo_version_1/test_1.py
+++ practica2/codigo_version_1/test_1.py
@@ -5,18 +5,18 @@ import api_v1
 
 def test_api_v1_1():
     n = 2
-    salida = api_v1.modelo_arima_completo_construido(n, './modeloT.pckl', './modeloH.pckl')
+    salida = api_v1.modelo_arima_completo_construido(n, '/tmp/workflow/v1/modeloT.pckl', '/tmp/workflow/v1/modeloH.pckl')
     assert  len(salida) == 2
 
 def test_api_v1_2():
     n = 2
-    modelo_v1_t = api_v1.recuperar_modelo('./modeloT.pckl')
-    modelo_v1_h = api_v1.recuperar_modelo('./modeloH.pckl')
+    modelo_v1_t = api_v1.recuperar_modelo('/tmp/workflow/v1/modeloT.pckl')
+    modelo_v1_h = api_v1.recuperar_modelo('/tmp/workflow/v1/modeloH.pckl')
     pred_v1_t = api_v1.funcion_predecir_v1(modelo_v1_t, n )
     pred_v1_h = api_v1.funcion_predecir_v1(modelo_v1_h, n) 
     dias, temp, hum = api_v1.prediccion_to_diccionario(n, pred_v1_t, pred_v1_h)
     dic = api_v1.crear_diccionario_salida(dias, temp, hum)
-    salida = api_v1.modelo_arima_completo_construido(n, './modeloT.pckl', './modeloH.pckl')
+    salida = api_v1.modelo_arima_completo_construido(n, '/tmp/workflow/v1/modeloT.pckl', '/tmp/workflow/v1/modeloH.pckl')
     assert  dic == salida
     assert len(dic) == len(salida)
 
